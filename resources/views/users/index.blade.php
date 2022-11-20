e7cedee2581f3d29efe7b5591b37b199472c043f
Ismail Hossain <ismail.63k@gmail.com>
2022-11-20 23:55:11 +0600

    Learning cart added

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -290,6 +290,98 @@
         <!--/ Container end -->
     </section>
     <!-- Facts end -->
+    <section id="news" class="news">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-12">
+
+                    <h3 class="section-sub-title">Learn</h3>
+                </div>
+            </div>
+            <!--/ Title row end -->
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="latest-post">
+                        <div class="latest-post-media">
+                            <a href="news-single.html" class="latest-post-img">
+                                <img loading="lazy" class="img-fluid" src="user/images/learn/SQL.jpg" alt="img" />
+                            </a>
+                        </div>
+                        <div class="post-body">
+                            <h4 class="post-title">
+                                <a href="news-single.html" class="d-inline-block">We Just Completes $17.6 million
+                                    Medical Clinic in Mid-Missouri</a>
+                            </h4>
+                            <div class="latest-post-meta">
+                                <span class="post-item-date">
+                                    <i class="fa fa-clock-o"></i> July
+                                    20, 2017
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Latest post end -->
+                </div>
+                <!-- 1st post col end -->
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="latest-post">
+                        <div class="latest-post-media">
+                            <a href="news-single.html" class="latest-post-img">
+                                <img loading="lazy" class="img-fluid" src="user/images/learn/XSS.png" alt="img" />
+                            </a>
+                        </div>
+                        <div class="post-body">
+                            <h4 class="post-title">
+                                <a href="news-single.html" class="d-inline-block">Thandler Airport Water Reclamation
+                                    Facility Expansion Project Named</a>
+                            </h4>
+                            <div class="latest-post-meta">
+                                <span class="post-item-date">
+                                    <i class="fa fa-clock-o"></i> June
+                                    17, 2017
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Latest post end -->
+                </div>
+                <!-- 2nd post col end -->
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="latest-post">
+                        <div class="latest-post-media">
+                            <a href="news-single.html" class="latest-post-img">
+                                <img loading="lazy" class="img-fluid" src="user/images/learn/broken.jpg" alt="img" />
+                            </a>
+                        </div>
+                        <div class="post-body">
+                            <h4 class="post-title">
+                                <a href="news-single.html" class="d-inline-block">Silicon Bench and Cornike Begin
+                                    Construction Solar Facilities</a>
+                            </h4>
+                            <div class="latest-post-meta">
+                                <span class="post-item-date">
+                                    <i class="fa fa-clock-o"></i> Aug
+                                    13, 2017
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Latest post end -->
+                </div>
+                <!-- 3rd post col end -->
+            </div>
+            <!--/ Content row end -->
+
+            <div class="general-btn text-center mt-4">
+                <a class="btn btn-primary" href="news-left-sidebar.html">See All Posts</a>
+            </div>
+        </div>
+        <!--/ Container end -->
+    </section>
+    <!--/ Learn end -->
 
     <section id="ts-service-area" class="ts-service-area pb-0">
         <div class="container">
