56fdd20b897617303b61350ae5c7c01e44bab248
Ismail Hossain <ismail.63k@gmail.com>
2023-01-04 21:43:40 +0600

    footer 100% done

--- resources/views/components/users/layouts/partials/footer.blade.php
+++ resources/views/components/users/layouts/partials/footer.blade.php
@@ -53,7 +53,51 @@
     </div>
     <!-- Footer main end -->
 
+    <div class="copyright">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="copyright-info">
+                        <span>Copyright &copy;
+                            <script>
+                                document.write(
+                                    new Date().getFullYear()
+                                );
+                            </script>
+                            All Rights Reserved by
+                            <a href="#">Cyberexpert</a>
+                        </span>
+                    </div>
+                </div>
 
+                <div class="col-md-6">
+                    <div class="footer-menu text-center text-md-right">
+                        <ul class="list-unstyled">
+                            <li><a href="#">About</a></li>
+                            <li>
+                                <a href="{{route('specialist')}}">Our Specialist</a>
+                            </li>
+                            <li><a href="{{route('questionanswer')}}">Q&A</a></li>
+                            <li>
+                                <a href="{{route('blog')}}">Blog</a>
+                            </li>
+                            <li>
+                                <a href="{{route('news')}}">News</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- Row end -->
+
+            <div id="back-to-top" data-spy="affix" data-offset-top="10" class="back-to-top position-fixed">
+                <button class="btn btn-primary" title="Back to Top">
+                    <i class="fa fa-angle-double-up"></i>
+                </button>
+            </div>
+        </div>
+        <!-- Container end -->
+    </div>
     <!-- Copyright end -->
 </footer>
 
