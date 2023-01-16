e3bcd462884b1d0b5593a5b2185b21186e2f6cf6
Ismail Hossain <ismail.63k@gmail.com>
2023-01-17 00:52:21 +0600

    unnecessary file deleting

--- resources/views/components/admin/layouts/master.blade.php
+++ resources/views/components/admin/layouts/master.blade.php
@@ -40,32 +40,18 @@
 
     <!-- jquery vendor -->
     <script src="/admin/js/lib/jquery.min.js"></script>
-    <script src="/admin/js/lib/jquery.nanoscroller.min.js"></script>
+
     <!-- nano scroller -->
     <script src="/admin/js/lib/menubar/sidebar.js"></script>
-    <script src="/admin/js/lib/preloader/pace.min.js"></script>
+
     <!-- sidebar -->
 
     <script src="/admin/js/lib/bootstrap.min.js"></script>
     <script src="/admin/js/scripts.js"></script>
     <!-- bootstrap -->
 
-    <script src="/admin/js/lib/calendar-2/moment.latest.min.js"></script>
-    <script src="/admin/js/lib/calendar-2/pignose.calendar.min.js"></script>
-    <script src="/admin/js/lib/calendar-2/pignose.init.js"></script>
 
 
-    <script src="/admin/js/lib/weather/jquery.simpleWeather.min.js"></script>
-    <script src="/admin/js/lib/weather/weather-init.js"></script>
-    <script src="/admin/js/lib/circle-progress/circle-progress.min.js"></script>
-    <script src="/admin/js/lib/circle-progress/circle-progress-init.js"></script>
-    <script src="/admin/js/lib/chartist/chartist.min.js"></script>
-    <script src="/admin/js/lib/sparklinechart/jquery.sparkline.min.js"></script>
-    <script src="/admin/js/lib/sparklinechart/sparkline.init.js"></script>
-    <script src="/admin/js/lib/owl-carousel/owl.carousel.min.js"></script>
-    <script src="/admin/js/lib/owl-carousel/owl.carousel-init.js"></script>
-    <!-- scripit init-->
-    <script src="/admin/js/dashboard2.js"></script>
 </body>
 
 </html>
